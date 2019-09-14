cb2484d23b8d31c4810f565ca433fc8235cf89df
Sebastián Gurín <sgurin@awalabs.com>
2019-09-14 16:32:55 -0300

    test-browser

--- mirada/test-browser/runTests.ts
+++ mirada/test-browser/runTests.ts
@@ -1,5 +1,6 @@
-import { rm, mkdir, exec, cp, mv, ShellString } from 'shelljs';
+import { rm, mkdir, exec, cp, mv, ShellString, config } from 'shelljs';
 import { equal, ok } from 'assert';
+config.silent = true
 
 const out = 'test-browser-outdir'
 exec('npm run build')
@@ -7,17 +8,24 @@ rm('-rf', out)
 mkdir('-p', out)
 cp('dist/mirada.min.js', out)
 cp('dist/src/opencv.js', out)
+cp('-r', 'test/assets/*', out)
 
-const tests:{file:string, assert:(s:ShellString)=>boolean}[] = [{
-  file: 'miradaBundle.html',
-  assert: s=>s.includes('General configuration for OpenCV')
-}]
+const tests: { file: string, assert: (s: ShellString) => boolean }[] = [
+  {
+    file: 'miradaBundle.html',
+    assert: s => s.includes('General configuration for OpenCV')
+  },
+  {
+    file: 'imageUtil.html',
+    assert: s => !s.includes('TEST ERROR:')
+  }
+]
 
-tests.forEach(t=>{
+tests.forEach(t => {
   rm('-rf', `${out}/index.html`)
   cp(`test-browser/${t.file}`, `${out}/index.html`)
   const cmd = 'npx ts-node test-browser/run.ts'
   const p = exec(cmd)
-  equal(p.code, 0, `Expected "${cmd}" without errors`)
-  ok(t.assert(p), 'Expected test assert OK')
+  equal(p.code, 0, `Expected "${cmd}" exit with status 0`)
+  ok(t.assert(p), 'Expected test assert OK - stdout was: \n'+p.stdout)
 })
